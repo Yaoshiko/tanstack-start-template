0bdb260cd56f93b2c443544f7e4e8eb1212577f8
Yaoshiko <nicola.barletta@outlook.it>
2025-02-16 10:34:50 +0100

    Fix root router context

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,4 +1,8 @@
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router';
+import {
+  Link,
+  Outlet,
+  createRootRouteWithContext
+} from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import { Meta, Scripts } from '@tanstack/start';
 import * as React from 'react';
@@ -7,63 +11,67 @@ import { NotFound } from '~/components/NotFound';
 import appCss from '~/styles/app.css?url';
 import { seo } from '~/utils/seo';
 import { fetchUser } from '~/auth/api';
+import { QueryClient } from '@tanstack/react-query';
 
-export const Route = createRootRoute({
-  // FIXME: Fix head.
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      ...seo({
-        title: 'WebApp starter',
-        description: `TanStack generic webapp starter.`
-      })
-    ],
-    links: [
-      { rel: 'stylesheet', href: appCss },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png'
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png'
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png'
-      },
-      { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
-      { rel: 'icon', href: '/favicon.ico' }
-    ]
-  }),
-  beforeLoad: async () => {
-    const user = await fetchUser();
+export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
+  {
+    // FIXME: Fix head.
+    head: () => ({
+      meta: [
+        {
+          charSet: 'utf-8'
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1'
+        },
+        ...seo({
+          title: 'WebApp starter',
+          description: `TanStack generic webapp starter.`
+        })
+      ],
+      links: [
+        { rel: 'stylesheet', href: appCss },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png'
+        },
+        { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
+        { rel: 'icon', href: '/favicon.ico' }
+      ]
+    }),
+    beforeLoad: async ({ context }) => {
+      const user = await fetchUser();
 
-    return {
-      user
-    };
-  },
-  errorComponent: (props) => {
-    return (
-      <RootDocument>
-        <DefaultCatchBoundary {...props} />
-      </RootDocument>
-    );
-  },
-  notFoundComponent: () => <NotFound />,
-  component: RootComponent
-});
+      return {
+        ...context,
+        user
+      };
+    },
+    errorComponent: (props) => {
+      return (
+        <RootDocument>
+          <DefaultCatchBoundary {...props} />
+        </RootDocument>
+      );
+    },
+    notFoundComponent: () => <NotFound />,
+    component: RootComponent
+  }
+);
 
 function RootComponent() {
   return (
