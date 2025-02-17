11d62792f188e233736ae3136df346675b7378ff
Yaoshiko <nicola.barletta@outlook.it>
2025-02-17 19:43:25 +0100

    Fix upgrade to Tailwind v4

--- app/lib/components/login-form.tsx
+++ app/lib/components/login-form.tsx
@@ -36,7 +36,7 @@ export function LoginForm({
           Login
         </Button>
         <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-          <span className="bg-background relative z-10 px-2 text-red-500">
+          <span className="bg-background relative z-10 px-2">
             Or continue with
           </span>
         </div>
