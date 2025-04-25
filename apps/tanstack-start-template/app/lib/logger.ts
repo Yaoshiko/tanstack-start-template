a887cc541929fb2d75a36d7d17a13e4aa0a6a55f
Yaoshiko <nicola.barletta@outlook.it>
2025-04-25 15:33:55 +0200

    Docker push

--- apps/tanstack-start-template/app/lib/logger.ts
+++ apps/tanstack-start-template/app/lib/logger.ts
@@ -3,6 +3,7 @@ import pino from 'pino';
 
 export function useLogger() {
   const { serverEnv, clientEnv } = useEnvironment();
+
   const logger = pino({
     level: serverEnv?.LOG_LEVEL ?? clientEnv?.VITE_LOG_LEVEL ?? 'info'
   });
