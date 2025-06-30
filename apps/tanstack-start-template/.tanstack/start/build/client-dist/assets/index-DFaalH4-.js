7f4737190f653594d8525254b8e147823b9a8c62
Yaoshiko <nicola.barletta@outlook.it>
2025-06-30 16:54:14 +0200

    WIP deploy pipeline

--- apps/tanstack-start-template/.tanstack/start/build/client-dist/assets/index-DFaalH4-.js
+++
@@ -1,1 +0,0 @@
-import{r as o,j as s}from"./main-D0n-BFDQ.js";import{P as i}from"./index-C-GjXKtG.js";var l="Label",a=o.forwardRef((r,e)=>s.jsx(i.label,{...r,ref:e,onMouseDown:t=>{t.target.closest("button, input, select, textarea")||(r.onMouseDown?.(t),!t.defaultPrevented&&t.detail>1&&t.preventDefault())}}));a.displayName=l;var m=a;export{a as L,m as R};
