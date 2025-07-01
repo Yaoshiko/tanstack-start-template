f0216f2e8d3e8f6c9b4b68459b57c06d32869097
Yaoshiko <nicola.barletta@outlook.it>
2025-07-01 16:48:18 +0200

    Again

--- apps/tanstack-start-template/src/routes/_navbar/index.tsx
+++ apps/tanstack-start-template/src/routes/_navbar/index.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute('/_navbar/')({
 });
 
 function Home() {
-  // Dummy change 10.
+  // Dummy change 11.
   return (
     <div className="container mx-auto">
       <section className="relative overflow-hidden px-8 py-32">
