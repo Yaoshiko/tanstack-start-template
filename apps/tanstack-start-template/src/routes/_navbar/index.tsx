0d0a3fad9c789d3658a96aa3ab5046bf6c53d1f8
Yaoshiko <nicola.barletta@outlook.it>
2025-07-14 11:58:04 +0200

    Refactor pipelines

--- apps/tanstack-start-template/src/routes/_navbar/index.tsx
+++ apps/tanstack-start-template/src/routes/_navbar/index.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute('/_navbar/')({
 });
 
 function Home() {
-  // Dummy change 15.
+  // Dummy change 16.
   return (
     <div className="container mx-auto">
       <section className="relative overflow-hidden px-8 py-32">
